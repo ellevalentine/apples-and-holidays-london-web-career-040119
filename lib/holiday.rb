cdcf9862ba9bc2fb160120ebba390cc7f92f4459
Elle Valentine <elle.valentine@hotmail.com>
2019-03-29 18:54:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,7 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   
-  (holiday_supplies)
+  holiday_supplies.each 
   holiday_supplies[:winter]
   
   # add the second argument, which is a supply, to BOTH the
