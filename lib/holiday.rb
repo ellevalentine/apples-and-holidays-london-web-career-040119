13080aa7c2f6b6e9200c20fe684035f64e70a03e
Elle Valentine <elle.valentine@hotmail.com>
2019-03-29 18:39:03 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,9 +17,8 @@ holiday_supplies={
    }
    
 
-def second_supply_for_fourth_of_july(holiday_hash)
-  
-   return second_supply_for_fourth_of_july[:summer][:fourth_of_july]
+def second_supply_for_fourth_of_july(holiday_supplies)
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
